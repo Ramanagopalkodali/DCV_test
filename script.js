fbef400566125d85ab9145891aa7d51565ed41e2
Ramanagopalkodali <da24b010@smail.iitm.ac.in>
2025-11-07 17:40:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ themeToggle.addEventListener('click', () => {
 const datasetsMap = {
   'HIV': 'HIV_data.xlsx',
   'TB': 'TB_data.xlsx',
-  'Diabetes': 'Diabetes_data.xlsx',
+  'Diabetes': 'Diabetes.xlsx',
   'Dengue': 'Dengue_data.xlsx'
 };
 
