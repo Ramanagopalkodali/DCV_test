dc972b54bff059da9e6f188b50e867335bce756a
Ramanagopalkodali <da24b010@smail.iitm.ac.in>
2025-11-10 12:30:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@
     'HIV': 'HIV_data.xlsx',
     'TB': 'TB_data.xlsx',
     'Diabetes': 'Diabetes.xlsx',
-    'Dengue': 'Dengue_data.xlsx'
+    '': 'HepatitisB.xlsx'
   };
 
   const diseaseSelect = document.getElementById('diseaseSelect');
